66f2f6ef890a1ab1a733b07081e02cfff8255860
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-11-10 15:50:06 +0900

    add: enable to push viewController into navigationViewController

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -8,6 +8,7 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    @window.rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
+    tapController = TapController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(tapController)
   end
 end
--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -3,11 +3,25 @@ class TapController < UIViewController
     super
 
     self.view.backgroundColor = UIColor.whiteColor
+    initNavigation
 
     @label = UILabel.alloc.initWithFrame(CGRectZero)
-    @label.text = "Taps"
+    tapText = "Taps #{self.navigationController.viewControllers.count}"
+    self.title = tapText
+    @label.text = tapText
     @label.sizeToFit
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
     self.view.addSubview @label
+  end
+
+  def initNavigation
+
+    rightButton =  UIBarButtonItem.alloc.initWithTitle "Push", style: UIBarButtonItemStyleBordered, target: self, action: 'push'
+    self.navigationItem.rightBarButtonItem = rightButton
+  end
+
+  def push
+    newController = TapController.alloc.initWithNibName nil, bundle: nil
+    self.navigationController.pushViewController newController, animated: true
   end
 end
